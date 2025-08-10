4c9bbb60c7652f5701b398382a7217191306e673
YYZhang <yuyangzhang2025@gmail.com>
2025-08-10 16:32:12 +0800

    language model finished

--- vlm/config.py
+++ vlm/config.py
@@ -13,3 +13,64 @@ class VLMConfig:
     vit_ln_eps: float = 1e-6
     vit_cls_flag: bool = False
     vit_model_type: str = "google/siglip2-base-patch16-512"
+    mp_pixel_shuffle_factor: int = 2
+    
+    lm_hidden_dim: int = 960
+    lm_inter_dim: int = 2560
+    lm_rms_eps: float = 1e-5
+    lm_re_base: int = 100000
+    lm_max_position_embeddings: int = 8192
+    lm_base_vocab_size: int = 49152
+    extra_token_amount: int = (
+        17  # Number of extra tokens for the VLM (image start, image end, image token)
+    )
+    lm_vocab_size: int = (
+        lm_base_vocab_size + extra_token_amount
+    )  # Not a great way to do this, but it works for now (vlm_extra_tokens cannot be a dict, since this is mutable, and a Field has no len() function)
+    lm_n_heads: int = 15
+    lm_n_kv_heads: int = 5
+    lm_dropout: float = 0.0
+    lm_n_blocks: int = 32
+    lm_attn_scaling: float = 1.0
+    lm_max_length: int = 1024
+    lm_use_tokens: bool = (
+        False  # Decide if the LM expects tokens or embeddings as input (if using as a backbone for the VLM, set to False)
+    )
+    lm_tie_weights: bool = (
+        True  # Decide if you want to tie the LM Head weight to the token embedding weights
+    )
+    lm_model_type: str = "HuggingFaceTB/SmolLM2-360M-Instruct"
+    lm_tokenizer: str = "HuggingFaceTB/SmolLM2-360M-Instruct"
+    lm_chat_template: str = (
+        "{% for message in messages %}{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}{% endfor %}{% if add_generation_prompt %}{{ '<|im_start|>assistant\n' }}{% endif %}"
+    )
+
+    mp_pixel_shuffle_factor: int = 4
+    mp_image_token_length: int = 64
+
+    max_img_size: int = 1024
+
+    vlm_extra_tokens: dict[str, str] = field(
+        default_factory=lambda: {
+            "image_token": "<|image|>",
+            "r1c1": "<row_1_col_1>",
+            "r1c2": "<row_1_col_2>",
+            "r1c3": "<row_1_col_3>",
+            "r1c4": "<row_1_col_4>",
+            "r2c1": "<row_2_col_1>",
+            "r2c2": "<row_2_col_2>",
+            "r2c3": "<row_2_col_3>",
+            "r2c4": "<row_2_col_4>",
+            "r3c1": "<row_3_col_1>",
+            "r3c2": "<row_3_col_2>",
+            "r3c3": "<row_3_col_3>",
+            "r3c4": "<row_3_col_4>",
+            "r4c1": "<row_4_col_1>",
+            "r4c2": "<row_4_col_2>",
+            "r4c3": "<row_4_col_3>",
+            "r4c4": "<row_4_col_4>",
+        }
+    )
+    vlm_load_backbone_weights: bool = True
+    vlm_checkpoint_path: str = "checkpoints"
+    hf_repo_name: str = "nanoVLM"
